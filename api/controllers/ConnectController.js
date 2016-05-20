f8574ba14cede2d00f398c9a2cf5070c135d3d97
Vladyslav Kopylash <vkop@grdnz.com>
2016-05-21 00:05:56 +0300

    join socket clients by customer when connecting

--- api/controllers/ConnectController.js
+++ api/controllers/ConnectController.js
@@ -8,11 +8,15 @@ module.exports = {
     return Plug.findOne({
       where: {
         id: req.body.id
+      },
+      include: {
+        model: Customer,
+        as: 'customer'
       }
     }).then(function(plug) {
       if (plug != null) {
-        sails.sockets.join(req, 'funSockets');
-        sails.sockets.broadcast('funSockets', 'greeting', req.body.id, req);
+        sails.sockets.join(req, plug.customer.id);
+        sails.sockets.broadcast(plug.customer.id, 'greeting', req.body.id, req);
         return res.ok('йоу');
       }
       return res.forbidden('WRONG UUID of socket plug');
